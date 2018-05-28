c2782edcf3bfb518a5b35e9dfe609a4d44779eb1
ScottWorks <scottworks.io@gmail.com>
2018-05-28 14:52:03 -0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+// Required
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const massive = require('massive');
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static(`${__dirname}/../build`));
+
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+
+massive(CONNECTION_STRING)
+  .then((dbInstance) => {
+    app.set('db', dbInstance);
+  })
+  .catch((e) => console.log(`Error: ${e}`));
+
+// ##### ENDPOINTS ######
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Creeping on Port: ${SERVER_PORT}`);
+});
