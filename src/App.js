9815d7cc298004a88ed8221595d8f15f3b907fca
brownrnoah <brownrnoah@gmail.com>
2018-05-29 17:00:37 -0600

    Updating Header.js and Footer.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Footer from "./components/components/Footer/Footer";
+import Header from "./components/components/Header/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <Footer />
       </div>
     );
   }
