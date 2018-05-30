2061a12098ebd22fa0524b729d183c85c4ad10ee
reaganforonda <foronda.reagan@gmail.com>
2018-05-30 11:44:46 -0600

    added sign uri function to s3 controller

---
+++ server/controllers/S3Controller.js
@@ -0,0 +1,43 @@
+const aws = require('aws-sdk');
+require('dotenv').load();
+
+const {
+    ACCESSKEYID,
+    SECRETACCESSKEY,
+    S3BUCKET
+} = process.env
+
+
+aws.config.update({
+    accessKeyId : ACCESSKEYID,
+    secretAccessKey : SECRETACCESSKEY,
+})
+
+module.exports = {
+
+    // FUNCTION TO GET SIGN URI
+    sign : (req, res) => {
+        let s3 = new aws.S3();
+
+        const {filename, filetype} = req.body
+
+        let params = {
+            Bucket : S3BUCKET,
+            Key : filename,
+            Expires : 60,
+            ContentType : filetype,
+            ACL : 'public-read'
+        }
+
+        s3.getSignedUrl('putObject', params, (err, data) => {
+            if(err) {
+                console.log(`Error at s3Controller ${err}`);
+                res.sendStatus(500);
+            } else {
+                res.status(200).send(data);
+            }
+        })
+    },
+
+    
+}
