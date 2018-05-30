3209bfdf308ca8913e2f75a23b400b6c0cfebe85
ScottWorks <scottworks.io@gmail.com>
2018-05-29 22:23:01 -0600

    updated getBaskets and seed file

--- server/controllers/NonProfitController.js
+++ server/controllers/NonProfitController.js
@@ -1,8 +1,13 @@
 module.exports = {
   getBaskets: (req, res) => {
     const db = req.app.get('db');
+    // const currentTime = Date.now()/1000;
+    const currentTime = 1527647830;
 
-    db.getBaskets().then((baskets) => {
+    console.log(currentTime);
+
+    db.getBaskets([currentTime]).then((baskets) => {
+      console.log(baskets);
       res.status(200).send(baskets);
     });
   }
