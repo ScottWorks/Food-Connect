c8251bb675f895c0b4b0c49aaddd32b66f580255
brownrnoah <brownrnoah@gmail.com>
2018-05-30 17:07:38 -0600

    laying out barebones for landing page, and fixing animations

--- src/components/components/Header/Header.js
+++ src/components/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import Scone from "./../../../assets/images/scone.png";
 import './Header.css'
-// import {Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 class Header extends Component {
+    //I commented out this block of code, it because I am currently using a different hamburger menu.
 
     // toggleCSS() {
     //     document.getElementById('bar1').classList.toggle("change_bar1");
@@ -15,22 +16,22 @@ class Header extends Component {
         return (
             <div id="header_main">
                 <div id="header_contents">
-
+                
                 <div id="menuToggle">
                         <input type="checkbox" />
                         <span></span>
                         <span></span>
                         <span></span>
                         <ul id="menu">
-                            <a href="#"><li>Sign Up</li></a>
-                            <a href="#"><li>Login</li></a>
-                            <a href="#"><li>Dashboard</li></a>
-                            <a href="#"><li>FAQ</li></a>
+                            <Link to=""><li>Sign Up</li></Link>
+                            <Link to=""><li>Login</li></Link>
+                            <Link to="/nonprofit"><li>Dashboard</li></Link>
+                            <a href=""><li onClick={()=> this.props.handler()}>FAQ</li></a>
                         </ul>
                     </div>
 
-                    <img alt="scone" src={Scone} id="header_img" />
-
+                    <Link to="/"><img alt="scone" src={Scone} id="header_img" /></Link>
+                    {/*I am not using this hamburger right now.*/}
                     {/* <div id="container" onClick={() => this.toggleCSS()}>
                         <div id="bar1"></div>
                         <div id="bar2"></div>
