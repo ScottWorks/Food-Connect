7544c05bf19c8e7ded76ae424546c1adbc1e40ec
Tayt <tayt@Tayts-MacBook-Pro.local>
2018-05-30 14:24:24 -0600

    added new route for registration

---
+++ src/components/auth/Registration/Registration.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import {connect} from 'react-redux'
+
+class Register extends Component {
+    render() {
+        return(
+            <div>
+                registration view!
+            </div>
+        )
+    }
+}
+
+// let mapStateToProps = (state) => {
+//     return state
+// }
+
+export default (Register)
