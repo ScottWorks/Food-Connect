e3f23c50ce0c3ba3d6c9c3b889f558b2a48da701
jacoblafemadsen <jacoblafemadsen@gmail.com>
2018-05-30 10:34:15 -0600

    fixed to master

--- src/components/layouts/Business/BasketTable/BasketTable.js
+++ src/components/layouts/Business/BasketTable/BasketTable.js
@@ -14,11 +14,13 @@ class BasketTable extends React.Component {
   }
 
   prepToAddItem() {
+    var timeString = this.state.expirationDate + this.state.expirationTime
+    var parts = timeString.match(/(\d{2})\/(\d{2})\/(\d{4}) (\d{2}):(\d{2})/);
+    var utcTime = Date.UTC(+parts[3], parts[2]-1, +parts[1], +parts[4], +parts[5]);
     var itemObj = {
       name: this.state.name,
       weight: this.state.weight,
-      expirationDate: this.state.expirationDate,
-      expirationTime: this.state.expirationTime
+      utcTime: itemObj
     }
     this.props.addItemToBasket(itemObj)
   }
@@ -26,22 +28,29 @@ class BasketTable extends React.Component {
   render() {
     return (
       <div className="BasketTable">
-        <input 
-          placeholder="Item name"
-          onChange={e => this.setState({name: e.target.value})}
-        />
-        <input 
-          placeholder="Weight in pounds"
-          onChange={e => this.setState({weight: e.target.value})}
-        />
-        <DatePicker 
-          onChange={(x, date) => this.setState({expirationDate: date})}
-          hintText="Portrait Dialog"
-        />
-        <TimePicker
-          onChange={(x, time) => this.setState({expirationDate: time})}
-          hintText="12hr Format"
-        />
+        <button
+          onClick={() => this.props.makeBasket()}
+        >
+          Make Basket
+        </button>
+        <div>
+          <input 
+            placeholder="Item name"
+            onChange={e => this.setState({name: e.target.value})}
+          />
+          <input 
+            placeholder="Weight in pounds"
+            onChange={e => this.setState({weight: e.target.value})}
+          />
+          <DatePicker 
+            onChange={(x, date) => this.setState({expirationDate: date})}
+            hintText="Portrait Dialog"
+          />
+          <TimePicker
+            onChange={(x, time) => this.setState({expirationDate: time})}
+            hintText="12hr Format"
+          />
+        </div>
         <button
           onClick={() => this.prepToAddItem()}
         >Add Item</button>
