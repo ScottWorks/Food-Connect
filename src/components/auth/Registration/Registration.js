2019cbb69299b6eeb85373a33d05eee24a878240
Tayt <tayt@Tayts-MacBook-Pro.local>
2018-05-30 15:42:00 -0600

    adding more of the registration inputs

--- src/components/auth/Registration/Registration.js
+++ src/components/auth/Registration/Registration.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import {connect} from 'react-redux'
+// import {connect} from 'react-redux'
+import './registration.css'
 
 class Register extends Component {
     constructor() {
@@ -12,7 +13,13 @@ class Register extends Component {
             city: '',
             state: '',
             zip: '',
+            firstName: '',
+            lastName: '',
+            userName: '',
+            pw: '',
+            pwView: false
         }
+
         this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
     }
@@ -27,10 +34,20 @@ class Register extends Component {
         alert(`You are a ${this.state.organizationType}`)
     }
 
+    seePassword() {
+        this.setState({
+            pwView: !this.state.pwView
+        })
+    }
+
+    registerOrganization() {
+        
+    }
+
     render() {
         return(
             <div>
-                <div>
+                <section className='registry-containers'>
                     <span>What type of organization are you representing?</span>
                     <ul>
                         <li>
@@ -56,37 +73,85 @@ class Register extends Component {
                             </label>
                         </li>
                     </ul>
-                </div>
-                <button onClick={this.handleClick}>Continue</button>
+                    <button onClick={this.handleClick}>Continue</button>
+                </section>
                 
                 <br/>
-                <div>
+                <section className='registry-containers'>
                     <span>Where are you located?</span>
                     <div>
                         <span>Street Address</span>
                         <input type="text"
                             value={this.state.streetAddress}
+                            onChange={ (e) => this.setState({ streetAddress: e.target.value})}
                         />
                     </div>
                     <div>
                         <span>City</span>
                         <input type="text"
                             value={this.state.city}
+                            onChange={ (e) => this.setState({ city: e.target.value})}
                         />
                     </div>
                     <div>
                         <span>streetAddress</span>
                         <input type="text"
                             value={this.state.state}
+                            onChange={ (e) => this.setState({ state: e.target.value})}
                         />
                     </div>
                     <div>
                         <span>Zip</span>
                         <input type="text"
                             value={this.state.zip}
+                            onChange={ (e) => this.setState({ zip: e.target.value} )}
                         />
                     </div>
-                </div>
+                    <button>Continue</button>
+                </section>
+                <br/>
+                <section  className='registry-containers'>
+                        <span>Finally Lets figure out your name and how you want to log in.</span>
+                    <div>
+                        <span>First Name</span>
+                        <input 
+                            type="text"
+                            value={this.state.firstName}
+                            onChange={ (e) => this.setState({ firstName: e.target.value })}
+                            />
+                    </div>
+                    <div>
+                        <span>Last Name</span>
+                        <input 
+                            type="text"
+                            value={this.state.lastName}
+                            onChange={ (e) => this.setState({ lastName: e.target.value })}
+                            />
+                    </div>
+                    <div>
+                        <span>Username</span>
+                        <input 
+                            type="text"
+                            value={this.state.userName}
+                            onChange={ (e) => this.setState({ userName: e.target.value })}
+                            />
+                    </div>
+                    <div>
+                        <span>Password</span>
+                        <input 
+                            type={ (this.state.pwView) ? "text" : "password" }
+                            value={this.state.pw}
+                            onChange={ (e) => this.setState({ pw: e.target.value }) }
+                            /> <span onClick={() => this.seePassword()}>Switch</span>
+                    </div>
+                    <div>
+                        <span>Confirm Password</span>
+                        <input 
+                            type={ (this.state.pwView) ? "text" : "password" }
+                            /> <span onClick={ () => this.seePassword()}>Switch</span>
+                    </div>
+                </section>
+                <button onClick={this.registerOrganization}>Register</button>
             </div>
         )
     }
