af9e9e094493222e8fb96f8a90f6129b6333a2e2
Tayt <tayt@Tayts-MacBook-Pro.local>
2018-05-31 14:42:54 -0600

    finished registration logic

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -7,56 +7,56 @@ module.exports = {
         const db = req.app.get('db')
 
         let group;
-        if(organizationType === 'non-profit') {
-            group = await db.register_np([organizationName, streetAddress, city, statee, zip, phoneNumber, specificType, firstName, lastName ])
+        if (organizationType === 'non-profit') {
+            group = await db.register_np([organizationName, streetAddress, city, statee, zip, phoneNumber, specificType, firstName, lastName])
             // .then( group => {
             //     res.status(200).send(group.data)
             //     console.log("We created a non-profit!")
             // })
             console.log('New np added to DB!', group)
 
-            db.check_username([userName]).then( user => {
-                if(user.length !== 0) {
-                  console.log('Please choose a different username.')
-                  res.status(200).send('Username taken. Please choose another, and try again.')
-                
+           await db.check_username([userName]).then(user => {
+                if (user.length !== 0) {
+                    console.log('Please choose a different username.')
+                    res.status(200).send('Username taken. Please choose another, and try again.')
+
                 } else {
                     const salt = bcrypt.genSaltSync(10)
                     const hash = bcrypt.hashSync(pw, salt)
-    
-                    db.register_np_admin([userName, hash, group.non_profit_id]).then( user => {
+
+                    db.register_np_admin([userName, hash, group[0].non_profit_id]).then(user => {
                         console.log('Np user created!')
                     })
                 }
             })
-    
+
         }
 
-        if(organizationType === 'business') {
+        if (organizationType === 'business') {
             group = await db.register_business([organizationName, streetAddress, city, statee, zip, phoneNumber, specificType, firstName, lastName])
             // .then( group => {
             //     console.log("We created a new business!")
             //     res.status(200).send(group.data)
             // })
             console.log('New business added to DB!')
-            
-            db.check_username([userName]).then( user => {
-                if(user.length !== 0) {
-                  console.log('Please choose a different username.')
-                  res.status(200).send('Username taken. Please choose another, and try again.')
-                
+
+           await db.check_username([userName]).then(user => {
+                if (user.length !== 0) {
+                    console.log('Please choose a different username.')
+                    res.status(200).send('Username taken. Please choose another, and try again.')
+
                 } else {
                     const salt = bcrypt.genSaltSync(10)
                     const hash = bcrypt.hashSync(pw, salt)
-    
-                    db.register_business_admin([userName, hash, group.business_id]).then( user => {
+
+                    db.register_business_admin([userName, hash, group[0].business_id]).then(user => {
                         console.log('Business user created!')
                     })
                 }
             })
-    
+
         }
-        
+
     }
 }
 
