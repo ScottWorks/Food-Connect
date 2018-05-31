510eb6f663880b88d007f3a036bca4b4ecadaadd
reaganforonda <foronda.reagan@gmail.com>
2018-05-31 11:06:15 -0600

    added a function to pull all completed basket for a business for analytics

--- server/controllers/analyticsController.js
+++ server/controllers/analyticsController.js
@@ -8,5 +8,17 @@ module.exports = {
             console.log(`Error while fetching all baskets from DB: ${err}`);
             res.sendStatus(500);
         })
+    },
+
+    getBussinessCompletedBaskets : (req, res) => {
+        const dbInstance = req.app.get('db');
+        const {businessID} = req.params;
+
+        dbInstance.analytics_businessBaskets([businessID]).then((baskets) => {
+            res.status(200).send(baskets)
+        }).catch((err) => {
+            console.log(`Error while fetching all baskets for a business: ${err}`);
+            res.sendStatus(500);
+        })
     }
 }
