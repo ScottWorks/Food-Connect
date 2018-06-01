c0c4b265c546725e3a72f9eb66f7a58bbb9ff3e5
reaganforonda <foronda.reagan@gmail.com>
2018-05-31 18:51:29 -0600

    boiler plate Bar Chart

---
+++ src/components/components/Stats/StatChart.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Bar} from 'react-chartjs-2';
+
+
+export default class StatChart extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            
+        }
+    }
+
+
+    render() {
+        return (
+            <Bar/>
+        )
+    }
+}
