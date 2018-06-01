810a1048df0b63dd6b3a3f80e3093c2e22b0da09
reaganforonda <foronda.reagan@gmail.com>
2018-06-01 11:19:29 -0600

    fixed controller

--- server/controllers/analyticsController.js
+++ server/controllers/analyticsController.js
@@ -1,5 +1,6 @@
 module.exports = {
     getAllCompletedBaskets : (req, res) => {
+        
         const dbInstance = req.app.get('db');
 
         dbInstance.analytics_allBaskets().then((baskets) => {
@@ -11,11 +12,12 @@ module.exports = {
     },
 
     getBussinessCompletedBaskets : (req, res) => {
+        
         const dbInstance = req.app.get('db');
         const {businessID} = req.params;
 
-        dbInstance.analytics_businessBaskets([businessID]).then((baskets) => {
-            res.status(200).send(baskets)
+        dbInstance.analytics_businessBaskets([businessID]).then((result) => {
+            res.status(200).send(result)
         }).catch((err) => {
             console.log(`Error while fetching all baskets for a business: ${err}`);
             res.sendStatus(500);
