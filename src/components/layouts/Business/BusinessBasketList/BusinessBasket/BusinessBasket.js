69763e20a04e52e01a3836b46007acd78626c3a1
jacoblafemadsen <jacoblafemadsen@gmail.com>
2018-06-05 16:59:18 -0600

    rest of business view ready for css

--- src/components/layouts/Business/BusinessBasketList/BusinessBasket/BusinessBasket.js
+++ src/components/layouts/Business/BusinessBasketList/BusinessBasket/BusinessBasket.js
@@ -1,20 +1,57 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { deleteBasket, editBasket } from '../../../../../ducks/businessReducer'
+import { fromEpoch } from '../../../../../config/timeConversion'
 import './BusinessBasket.css'
 
 function BusinessBasket(props) {
+
   if(props.basket && props.basket.items) {
-    var items = props.basket.items.map((e, i) => {
+    var items = props.basket.items.map((e, i)=> {
       return (
-        <div className="bus-basket-item">
+        <div 
+          className="bus-basket-item"
+          key={e.item + i}
+        >
+          <p>Item:</p>
           <p>{e.item}</p>
+          <p>Weight:</p>
           <p>{e.weight}</p>
+          <p>Fair Market Value:</p>
           <p>{e.FMV}</p>
         </div>
       )
     })
   }
+
+  if(props.basket) {
+    var checkStatus = ''
+    switch(props.basket.status) {
+      case 0:
+        checkStatus = 'Pending' 
+        break;
+
+      case 1:
+        checkStatus = 'Completed'
+        break;
+
+      case 2:
+        checkStatus = 'Scheduled'
+        break;
+
+      case 3:
+        checkStatus = 'Expired'
+        break;
+
+      case 4:
+        checkStatus = 'No Pickup'
+        break;
+      
+      default:
+        checkStatus = ''
+    }
+  }
+
   return (
     <div className="BusinessBasket">
       <button
@@ -24,6 +61,8 @@ function BusinessBasket(props) {
         onClick={() => props.deleteBasket({index: props.index, id: props.basket.basket_id})}
       >Delete</button>
       <div>
+        <p>{props.basket ? checkStatus : ''}</p>
+        <p>{props.basket ? fromEpoch(props.basket.pick_up_time, 'ddd, MMM Do, h:mm a') : ''}</p>
         {items}
       </div>
     </div>
