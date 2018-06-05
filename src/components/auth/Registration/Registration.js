ab6d9467f468516ecc764d3c0cdba7cec046e6d2
reaganforonda <foronda.reagan@gmail.com>
2018-06-05 12:39:29 -0600

    styling and func registration form

--- src/components/auth/Registration/Registration.js
+++ src/components/auth/Registration/Registration.js
@@ -16,10 +16,11 @@ class Register extends Component {
             organizationType: '',
             organizationName: '',
             specificType: '',
-            streetAddress: '',
-            city: '',
-            statee: '',
-            zip: '',
+            addresss: '',
+            // streetAddress: '',
+            // city: '',
+            // statee: '',
+            // zip: '',
             firstName: '',
             lastName: '',
             phoneNumber: '',
@@ -217,23 +218,70 @@ class Register extends Component {
     }
 
     handlePanelHeader2Click(){
-        if(this.state.panel1State.panel2BodyState==='panel-body-collapsed'){
+        
+        if(this.state.panel2State.panel2BodyState==='panel-body-collapsed' && this.state.organizationType !== ''){
+
+            // Open Up Panel 2
             let tempPanel2State = Object.assign({}, this.state.panel2State);
             tempPanel2State.panel2BodyState = 'panel-body-extended';
             tempPanel2State.panel2HeaderState = 'panel-header';
             this.setState({panel2State:tempPanel2State})
+
+            // Collapse Panels 1,3, & 4
+            let tempPanel1State = Object.assign({}, this.state.panel1State);
+            tempPanel1State.panel1BodyState = 'panel-body-collapsed';
             let tempPanel3State = Object.assign({}, this.state.panel3State);
             tempPanel3State.panel3BodyState = 'panel-body-collapsed';
-            this.setState({panel3State: tempPanel3State})
+            let tempPanel4State = Object.assign({}, this.state.panel4State);
+            tempPanel4State.panel4BodyState = 'panel-body-collapsed';
+            this.setState({panel1State: tempPanel1State, panel3State: tempPanel3State, panel4State: tempPanel4State})
         }
     }
 
     handlePanelHeader3Click(){
-        
+        if(this.state.panel3State.panel3BodyState==='panel-body-collapsed'
+            && this.state.organizationName !== '' 
+            && this.state.organizationType !== ''
+            && this.state.specificType !==''){
+            // Open Up Panel 3
+            let tempPanel3State = Object.assign({}, this.state.panel3State);
+            tempPanel3State.panel3BodyState = 'panel-body-extended';
+            tempPanel3State.panel3HeaderState = 'panel-header';
+            this.setState({panel3State:tempPanel3State})
+
+            // Collapse Panels 1,2, & 4
+            let tempPanel1State = Object.assign({}, this.state.panel1State);
+            tempPanel1State.panel1BodyState = 'panel-body-collapsed';
+            let tempPanel2State = Object.assign({}, this.state.panel2State);
+            tempPanel2State.panel2BodyState = 'panel-body-collapsed';
+            let tempPanel4State = Object.assign({}, this.state.panel4State);
+            tempPanel4State.panel4BodyState = 'panel-body-collapsed';
+            this.setState({panel1State: tempPanel1State, panel2State: tempPanel2State, panel4State: tempPanel4State})
+        }
     }
 
     handlePanelHeader4Click(){
+        if(this.state.panel4State.panel4BodyState==='panel-body-collapsed'
+            && this.state.organizationName !==''
+            && this.state.organizationType !==''
+            && this.state.specificType !==''
+            && this.state.addresss !==''){
 
+            // Open Up Panel 4
+            let tempPanel4State = Object.assign({}, this.state.panel4State);
+            tempPanel4State.panel4BodyState = 'panel-body-extended';
+            tempPanel4State.panel4HeaderState = 'panel-header';
+            this.setState({panel4State:tempPanel4State})
+
+            // Collapse Panels 1,2, & 3
+            let tempPanel1State = Object.assign({}, this.state.panel1State);
+            tempPanel1State.panel1BodyState = 'panel-body-collapsed';
+            let tempPanel2State = Object.assign({}, this.state.panel2State);
+            tempPanel2State.panel2BodyState = 'panel-body-collapsed';
+            let tempPanel3State = Object.assign({}, this.state.panel3State);
+            tempPanel3State.panel3BodyState = 'panel-body-collapsed';
+            this.setState({panel1State: tempPanel1State, panel2State: tempPanel2State, panel3State: tempPanel3State})
+        }
     }
 
 
@@ -278,58 +326,79 @@ class Register extends Component {
                                 Organization Type</div>
                         <div className={this.state.panel1State.panel1BodyState}>
                             <h2>What type of organizationare you representing?</h2>
-                            <form>
+                            <form className='registration-form'>
+                            <div>
                                 <input required='true' type='radio' checked={this.state.organizationType==='non-profit'} 
                                         value='non-profit' onChange={this.handleChange} id='non-profit'/>
                                 <label for='non-profit'>Non-Profit</label>
+                                </div>
+                                <div>
                                 <input type='radio' checked={this.state.organizationType==='business'} 
                                         onChange={this.handleChange}  value='business' id='business'/>
-                                <label for='business'>Business</label>
-                                <input type='submit' onClick={()=> this.handleContinueClickPanel1()} value='Continue'/>
+                                <label for='business'>Business</label></div>
+                                <input className='form-continue-button' type='submit' onClick={()=> this.handleContinueClickPanel1()} value='Continue'/>
                             </form>     
                         </div>
                     </section>
 
                     <section className='question-panel'>
-                        <button className={this.state.panel2State.panel2HeaderState}
-                                
+                        <div className={this.state.panel2State.panel2HeaderState}
+                                onClick={()=>this.handlePanelHeader2Click()}
                         >
-                        Organization Info</button>
+                        Organization Info</div>
                         <div className={this.state.panel2State.panel2BodyState}>
                             <h2>Tell us a little more about your organization.</h2>
-                            <form>
-                                <p>Organization Name</p>
-                                <input required='true' placeholder='Organization Name' />
-                                <p>Type of Business</p>
-                                <input required='true' />
-                                <input type='submit' value='Continue' onClick={()=>this.handleContinueClickPanel2()}/>
+                            <form className='registration-form'>
+                                <input className='form-input-box'
+                                    onChange={(e) => this.setState({organizationName: e.target.value})}
+                                    required='true' placeholder='Enter Organization Name' 
+                                    type='text'/>
+                                <input className='form-input-box' 
+                                    onChange={(e)=> this.setState({specificType: e.target.value})}
+                                    required='true' placeholder={`Enter Type of ${this.state.organizationType}`}
+                                    type='text'/>
+                                <input className='form-continue-button' type='submit' value='Continue' onClick={()=>this.handleContinueClickPanel2()}/>
                             </form>
                         </div>
                     </section>
 
                     <section className='question-panel'>
-                        <button className={this.state.panel3State.panel3HeaderState}>Location</button>
+                        <div className={this.state.panel3State.panel3HeaderState}
+                                onClick={()=>this.handlePanelHeader3Click()}>Location</div>
                         <div className={this.state.panel3State.panel3BodyState}>
                             <h2>Where are you located?</h2>
-                            <form>
-                                <span>Address:</span><input />
-                                <input onClick={()=>this.handleContinueClickPanel3()} type='submit' value='Continue'/>
+                            <form className='registration-form'>
+                                <input className='form-input-box'
+                                    onChange={(e)=>this.setState({addresss: e.target.value})}
+                                    required='true' placeholder='Enter Address'
+                                    type='text'/>
+                                <input className='form-continue-button' onClick={()=>this.handleContinueClickPanel3()} type='submit' value='Continue'/>
                             </form>
                         </div>
                     </section>
 
                     <section className='question-panel'>
-                        <button className={this.state.panel4State.panel4HeaderState}>Details</button>
+                        <div className={this.state.panel4State.panel4HeaderState}
+                            onClick={()=>this.handlePanelHeader4Click()}
+                        >Details</div>
                         <div className={this.state.panel4State.panel4BodyState}>
                             <h2>Tell us the details</h2>
-                            <form>
-                                <span>First Name:</span><input required='true' />
-                                <span>Last NAme: </span><input required='true'/>
-                                <span>Phone Number:</span> <input required='true'/>
-                                <span>Username:</span> <input required='true'/>
-                                <span>Password:</span> <input required='true'/>
-                                <span>Confirm Passwodr:</span><input required='true'/>
-                                <input type='submit' value='Register'/>
+                            <form className='registration-form'>
+                                <input onChange={(e)=>this.setState({firstName:e.target.value})} 
+                                    value={this.state.firstName} className='form-input-box' required='true'
+                                    placeholder='First Name'/>
+                                <input onChange={(e)=>this.setState({lastName: e.target.value})} 
+                                    value={this.state.lastName} className='form-input-box' required='true'
+                                    placeholder='Last Name'/>
+                                <input onChange={(e)=>this.setState({phoneNumber: e.target.value})} 
+                                    value={this.state.phoneNumber} className='form-input-box' required='true' 
+                                    placeholder='Phone Number'/>
+                                <input onChange={(e)=>this.setState({userName: e.target.value})} 
+                                    value={this.state.userName} className='form-input-box' required='true' 
+                                    placeholder='Username'/>
+                                <input className='form-input-box' required='true' placeholder='Password'/>
+                                <input className='form-input-box' required='true' placeholder='Confirm Password'/>
+                                <input className='form-continue-button' type='submit' value='Register'/>
                             </form>
                         </div>
                     </section>
