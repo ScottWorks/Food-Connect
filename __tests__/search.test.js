78390ec6b041b78e3b19854a8c87753faea515b9
ScottWorks <scottworks.io@gmail.com>
2018-06-13 15:41:29 -0600

    changed Reserve to conditionally render

--- __tests__/search.test.js
+++ __tests__/search.test.js
@@ -1,23 +1,12 @@
 //A test to test functionality of the searchBasket Function in searchUtil.js.
-
 let searchFunctions = require('../src/config/searchUtil.js');
 
-test('', () => {
+test('', () => {});
 
-})
+test('', () => {});
 
-test('', () => {
+test('', () => {});
 
-})
+test('', () => {});
 
-test('', () => {
-
-})
-
-test('', () => {
-
-})
-
-test('', () => {
-
-})
+test('', () => {});
