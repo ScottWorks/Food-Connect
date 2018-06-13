9a5514abc4eef1ddce82dbbc4ff5a42c4fb3c65e
reaganforonda <foronda.reagan@gmail.com>
2018-06-13 08:32:41 -0600

    testing

--- cypress/integration/reagan_test_cypress.js
+++ cypress/integration/reagan_test_cypress.js
@@ -1,7 +1,5 @@
 /*
- * 
  * Test Registration Page
- * 
  */
 
 //  Can Visit Registration Page and continue on to the next Registration form Panel
@@ -18,7 +16,6 @@ it('Test Registration Page', ()=> {
 })
 
 /*
- *
  * Test Login Page
  * Can login to Non-Profit View
  */
@@ -32,8 +29,33 @@ it('Test Login', ()=> {
 });
 
 // Test Add to Wishlist
-// it('Test Adding to Wishlist', ()=> {
-//     cy.get('.wishlist-add-container').get('form').get('input[placeholder="Add Items..."]').type('Bread').type('{enter}')
-// })
+it('Test Adding to Wishlist', ()=> {
+    cy.get('.wishlist-add-container').get('form').get('input[placeholder="Add Items..."]').type('Bread').type('{enter}')
+})
 
-// Can Logout
+/*
+ * Test Login Page
+ * Can login to Business View
+ */
+it('Test Login', ()=> {
+    cy.visit('http://localhost:3000/#/login');
+    cy.get('.login-form').get('input[name="username"]').type('Mark')
+    cy.get('.login-form').get('input[name="username"]').next().next().type('test');
+    cy.get('.login-form').get('button[type="submit"]').click();
+    cy.url().should('eq', 'http://localhost:3000/#/business', {timeout: 10000});
+});
+
+/*
+ * Test Login Page
+ * Can login to Business View
+ * And Click on Make a Basket
+ */
+
+ it('Test Make a Basket Click', ()=> {
+    cy.visit('http://localhost:3000/#/login');
+    cy.get('.login-form').get('input[name="username"]').type('Mark')
+    cy.get('.login-form').get('input[name="username"]').next().next().type('test');
+    cy.get('.login-form').get('button[type="submit"]').click();
+    cy.url().should('eq', 'http://localhost:3000/#/business', {timeout: 10000});
+    cy.get('.BaksetTable')
+ })
